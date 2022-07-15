6434492c727b94ca052f7afa9a6f9c5508df417a
Andrei Salauyou <Andrei_Salauyou@epam.com>
2022-07-15 11:39:08 +0300

    Fixed task

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const csv = require('csvtojson');
-const fs = require('fs');
-try {
-    console.log('script started');
-    csv({ delimiter: ';' })
-    .fromFile("./nodejs-hw1-ex1.csv")
-    .then(jsonObj => { 
-        try{
-            fs.writeFileSync('./hw1-ex2.txt',JSON.stringify(jsonObj)); 
-        }
-        catch(error) {
-            console.log(error);
-        }
-    }
-    )
-}
-catch (error) {
-    console.log(error);
-}
+const readableFS = require('fs').createReadStream("./nodejs-hw1-ex1.csv").pipe(csv({ delimiter: ';' }));
+const writableFS = require('fs').createWriteStream('./hw1-ex2.txt');
+
+readableFS.on('data', data => {
+    writableFS.write(data.toString());
+});
+
+readableFS.on('error', error => console.error(error.message));
+
+writableFS.on('ready', () => console.log('Complited'));
