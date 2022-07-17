9fdff07d1f26660a8a414f79d1d4cc862eaa9687
Andrei Salauyou <Andrei_Salauyou@epam.com>
2022-07-18 00:38:19 +0300

    fixed pr issues

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const csv = require('csvtojson');
-const readableFS = require('fs').createReadStream("csv/nodejs-hw1-ex1.csv").pipe(csv({ delimiter: ';' }));
-const writableFS = require('fs').createWriteStream('./hw1-ex2.txt');
-
-readableFS.on('data', data => {
-    writableFS.write(data.toString());
-});
-
-readableFS.on('error', error => console.error(error.message));
-
-writableFS.on('ready', () => console.log('Complited'));
+const readableFile = 'csv/nodejs-hw1-ex1.csv';
+const writableFile = './hw1-ex2.txt';
+if( require('fs').existsSync(readableFile) && require('fs').existsSync(writableFile)) {
+    const readableFS = require('fs').createReadStream(readableFile).pipe(csv({ delimiter: ';', colParser:{ Amount:"number", Price:"number"}, noheader: false, headers:['book','author','amount','price'], }));
+    const writableFS = require('fs').createWriteStream(writableFile);
+    readableFS.on('data', data => {
+        writableFS.write(data.toString());
+    });
+    
+    readableFS.on('error', (error) => {
+        console.error(error.message);
+    });
+    
+    writableFS.on('ready', () => console.log('Complited'));
+}
+else {
+    console.log('Error - be sure you are used correct filepaths');
+}
--- index_es5.js
+++ index_es5.js
@@ -1,17 +1,22 @@
 'use strict';
 
 var csv = require('csvtojson');
-var readableFS = require('fs').createReadStream("csv/nodejs-hw1-ex1.csv").pipe(csv({ delimiter: ';' }));
-var writableFS = require('fs').createWriteStream('./hw1-ex2.txt');
+var readableFile = 'csv/nodejs-hw1-ex1.csv';
+var writableFile = './hw1-ex2.txt';
+if (require('fs').existsSync(readableFile) && require('fs').existsSync(writableFile)) {
+    var readableFS = require('fs').createReadStream(readableFile).pipe(csv({ delimiter: ';', colParser: { Amount: "number", Price: "number" }, noheader: false, headers: ['book', 'author', 'amount', 'price'] }));
+    var writableFS = require('fs').createWriteStream(writableFile);
+    readableFS.on('data', function (data) {
+        writableFS.write(data.toString());
+    });
 
-readableFS.on('data', function (data) {
-    writableFS.write(data.toString());
-});
+    readableFS.on('error', function (error) {
+        console.error(error.message);
+    });
 
-readableFS.on('error', function (error) {
-    return console.error(error.message);
-});
-
-writableFS.on('ready', function () {
-    return console.log('Complited');
-});
+    writableFS.on('ready', function () {
+        return console.log('Complited');
+    });
+} else {
+    console.log('Error - be sure you are used correct filepaths');
+}
