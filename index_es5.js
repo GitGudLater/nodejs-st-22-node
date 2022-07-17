0ddd8774ee88766a99e0f74f55d94ebf53625987
Andrei Salauyou <Andrei_Salauyou@epam.com>
2022-07-18 01:22:21 +0300

    optimized code

--- index_es5.js
+++ index_es5.js
@@ -3,20 +3,8 @@
 var csv = require('csvtojson');
 var readableFile = 'csv/nodejs-hw1-ex1.csv';
 var writableFile = './hw1-ex2.txt';
-if (require('fs').existsSync(readableFile) && require('fs').existsSync(writableFile)) {
-    var readableFS = require('fs').createReadStream(readableFile).pipe(csv({ delimiter: ';', colParser: { Amount: "number", Price: "number" }, noheader: false, headers: ['book', 'author', 'amount', 'price'] }));
-    var writableFS = require('fs').createWriteStream(writableFile);
-    readableFS.on('data', function (data) {
-        writableFS.write(data.toString());
-    });
+var stream = require('stream');
 
-    readableFS.on('error', function (error) {
-        console.error(error.message);
-    });
-
-    writableFS.on('ready', function () {
-        return console.log('Complited');
-    });
-} else {
-    console.log('Error - be sure you are used correct filepaths');
-}
+stream.pipeline(require('fs').createReadStream(readableFile).pipe(csv({ delimiter: ';', colParser: { amount: "number", price: "number" }, noheader: false, headers: ['book', 'author', 'amount', 'price'] })), require('fs').createWriteStream(writableFile), function (error) {
+    if (error) console.error(error);
+});
